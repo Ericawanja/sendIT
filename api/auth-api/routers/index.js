a50f8bff4d1b039865470e81f4d08c2f9bc5fd19
Ericawanja <wanjaerica@gmail.com>
2023-01-04 11:27:39 +0300

    debugged the schema

--- api/auth-api/routers/index.js
+++ api/auth-api/routers/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const validator = require("../middlewares/validator")
 
 const register = require('../controllers/register')
 const login = require('../controllers/login')
+const { registerSchema } = require('../schemas/index,')
 
 
 
-router.post("/register", register)
+router.post("/register",validator(registerSchema), register)
 router.post("/login", login)
 
 module.exports = router
