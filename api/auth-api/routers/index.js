59c67107b006018143e4ff35c7529faa15745972
Ericawanja <wanjaerica@gmail.com>
2023-01-04 12:31:05 +0300

    login endpoint

--- api/auth-api/routers/index.js
+++ api/auth-api/routers/index.js
@@ -4,11 +4,11 @@ const validator = require("../middlewares/validator")
 
 const register = require('../controllers/register')
 const login = require('../controllers/login')
-const { registerSchema } = require('../schemas/index,')
+const { registerSchema, loginSchema } = require('../schemas/index,')
 
 
 
 router.post("/register",validator(registerSchema), register)
-router.post("/login", login)
+router.post("/login",validator(loginSchema), login)
 
 module.exports = router
