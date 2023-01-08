80f2913c56c493dbd97b7625ca0ddeb3f459157a
Ericawanja <wanjaerica@gmail.com>
2023-01-08 13:05:45 +0300

    fixed bug on create parcel endpoint

--- api/parcels-api/controllers/adminControllers.js
+++ api/parcels-api/controllers/adminControllers.js
@@ -1,4 +1,4 @@
-const shortid = require('shortid')
+const shortid = require("shortid");
 const { exec } = require("../helpers/db_connect");
 
 const createParcel = async (req, res) => {
@@ -9,23 +9,45 @@ const createParcel = async (req, res) => {
       .json({ message: "You do not have the priviledge to create a delivery" });
 
   try {
-    
-    const parcel = req.body
-    const orderId = shortid.generate()
+    const parcel = req.body;
+    const orderId = shortid.generate();
 
+     await exec("createOrUpdateParcel", { orderId, ...parcel });
 
-    await exec("createOrUpdateParcel", {orderId, ...parcel})
-
-   return res
-   .status(200)
-   .json({ message: "You have successfully created the parcel" });
-
+    return res
+      .status(200)
+      .json({ message: `You have successfully created the parcel` });
   } catch (error) {
     res.status(400).json({ error: error.originalError.info.message });
   }
 };
 
-const updateParcel = (req, res) => {};
+const updateParcel = async (req, res) => {
+  const loggedUser = req.info;
+  if (!loggedUser.isAdmin)
+    return res
+      .status(401)
+      .json({ message: "You do not have the priviledge to create a delivery" });
+
+  try {
+    const parcel = req.body;
+    const parcelInDB = await exec("getOneParcel", { orderId: parcel.orderId });
+
+    if (parcelInDB.length === 0)
+      return res
+        .status(404)
+        .json({ message: "The parcel does not exist" });
+
+
+    await exec("createOrUpdateParcel", { ...parcel });
+
+    return res
+      .status(200)
+      .json({ message: "You have successfully created the parcel" });
+  } catch (error) {
+    res.status(400).json({ error: error.originalError.info.message });
+  }
+};
 
 module.exports = {
   createParcel,
