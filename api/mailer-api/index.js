726b2cc42520d669ef7e7a3e97780a8b927b24c0
Ericawanja <wanjaerica@gmail.com>
2023-01-10 15:27:36 +0300

    node mailer testing without templates

--- api/mailer-api/index.js
+++ api/mailer-api/index.js
@@ -1,17 +1,32 @@
-const nodemailer = require("nodemailer")
-const ejs = require("ejs")
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const mailconfigurations={
-    host:'smtp.gmail.com',
-    port:587,
-    secure:false,
-    requireTLS:true,
-    auth:{
-        user:process.env.USER,
-        pass:process.env.EMAILPASS
-    }
-}
+const mailconfigurations = {
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.EMAILPASS,
+  },
+};
 
-const transporter = nodemailer.createTransport(mailconfigurations)
+const transporter = nodemailer.createTransport(mailconfigurations);
+
+const mailoptions = {
+  from: process.env.USER,
+  to: process.env.TO,
+  subject: "Hello ✔",
+  text: "Hello world?",
+  html: "<b>Hello world?</b>",
+};
+
+ transporter
+  .sendMail(mailoptions)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => console.log(error));
