12c91f6b2deed60e70e47bb9403d8a14488e7b06
Ericawanja <wanjaerica@gmail.com>
2023-01-10 11:17:17 +0300

    user profile received parcels

--- api/parcels-api/controllers/userControllers.js
+++ api/parcels-api/controllers/userControllers.js
@@ -14,9 +14,36 @@ const getAllYourParcels = async (req, res) => {
   }
 };
 
-const getParcelsSent = async (req, res) => {};
+const getParcelsSent = async (req, res) => {
+ const user = req.info
+  try {
+    const parcels = await exec("getCustomerParticularParcel", {
+      state:"sent",
+      customerId: user.id,
+    });
+    if (parcels.length === 0)
+      return res.status(404).json({ message: "No parcel found" });
+    res.status(200).json({ parcels });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 
-const getParcelsReceived = async (req, res) => {};
+};
+
+const getParcelsReceived = async (req, res) => {
+  const user = req.info
+  try {
+    const parcels = await exec("getCustomerParticularParcel", {
+      state:"received",
+      customerId: user.id,
+    });
+    if (parcels.length === 0)
+      return res.status(404).json({ message: "No parcel found" });
+    res.status(200).json({ parcels });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
 
 module.exports = {
     getAllYourParcels,
