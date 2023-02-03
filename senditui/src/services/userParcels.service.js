63b6c7be0b8c108e429b3182074940e0167c46f5
Ericawanja <wanjaerica@gmail.com>
2023-02-03 11:26:22 +0300

    fixed bug in getting data

--- senditui/src/services/userParcels.service.js
+++ senditui/src/services/userParcels.service.js
@@ -3,24 +3,29 @@ import axios from "axios";
 class UserParcels {
   constructor() {
     this.BASE_URL = `http://localhost:4000/profile/parcels`;
-    this.token = localStorage.getItem("token");
 
+    
     this.config = {
-      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      headers: {
+        Authorization: `Bearer ${
+          localStorage.getItem("token")
+        }`,
+      },
     };
   }
 
   async GetAllUserParcels() {
     let url = this.BASE_URL;
+    console.log(localStorage.getItem('token'));
     try {
       let parcels = await axios.get(url, this.config);
       return parcels;
     } catch (error) {
       console.log(error);
-        let message = 'An error occured'
-        return {data:null, message}
+      let message = "An error occured";
+      return { data: null, message };
     }
   }
 }
 
-export default new UserParcels()
+export default new UserParcels();
