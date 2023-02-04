07aa0332efc97ded0e6656289c71189bc916ca33
Ericawanja <wanjaerica@gmail.com>
2023-02-04 09:43:38 +0300

    loader

--- senditui/src/Components/Orders/index.jsx
+++ senditui/src/Components/Orders/index.jsx
@@ -2,99 +2,58 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserParcels } from "../../redux/thunks/userParcels.thunks";
+import Loader from "../Common/Loader/index";
 import "./orders.css";
 
 function Orders() {
   const dispatch = useDispatch();
 
-  const { parcels } = useSelector((state) => state.parcels);
+  const { parcels, loading } = useSelector((state) => state.parcels);
   console.log(parcels);
-  const dummyParcels = [
-    {
-      orderId: 9605,
-      from: "Kampala uganda",
-      destination: "Kisumu, kenya",
-      weight: 1.5,
-      price: 23467,
-      status: "delivered",
-    },
-    {
-      orderId: 9605,
-      from: "Kampala uganda",
-      destination: "Kisumu, kenya",
-      weight: 1.5,
-      price: 23467,
-      status: "delivered",
-    },
-    {
-      orderId: 9605,
-      from: "Kampala uganda",
-      destination: "Kisumu, kenya",
-      weight: 1.5,
-      price: 23467,
-      status: "delivered",
-    },
-    {
-      orderId: 9605,
-      from: "Kampala uganda",
-      destination: "Kisumu, kenya",
-      weight: 1.5,
-      price: 23467,
-      status: "delivered",
-    },
-    {
-      orderId: 9605,
-      from: "Kampala uganda",
-      destination: "Kisumu, kenya",
-      weight: 1.5,
-      price: 23467,
-      status: "delivered",
-    },
-    {
-      orderId: 9605,
-      from: "Kampala uganda",
-      destination: "Kisumu, kenya",
-      weight: 1.5,
-      price: 23467,
-      status: "delivered",
-    },
-  ];
+  let dummyL = true;
 
   useEffect(() => {
     dispatch(getUserParcels());
   }, []);
   return (
-    <div className="tableContainer">
-      <span className="searchInput">
-        <input type="text" placeholder="Search parcel..." />
-      </span>
-      <table>
-        <thead>
-          <tr>
-            <th>Order Id</th>
-            <th>From</th>
-            <th>Destination</th>
-            <th>Weight</th>
-            <th>Price</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {parcels.length > 0 && parcels.map((parcel, index) => {
-            return (
-              <tr key={index}>
-                <td>{parcel.orderId}</td>
-                <td>{parcel._from}</td>
-                <td>{parcel.destination}</td>
-                <td>{parcel.weight}</td>
-                <td>{parcel.price}</td>
-                <td>{parcel.status}</td>
+    <>
+      {dummyL ? (
+       <Loader/>
+      ) : (
+        <div className="tableContainer">
+          <span className="searchInput">
+            <input type="text" placeholder="Search parcel..." />
+          </span>
+          <table>
+            <thead>
+              <tr>
+                <th>Order Id</th>
+                <th>From</th>
+                <th>Destination</th>
+                <th>Weight</th>
+                <th>Price</th>
+                <th>Status</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+            </thead>
+            <tbody>
+              {parcels.length > 0 &&
+                parcels.map((parcel, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{parcel.orderId}</td>
+                      <td>{parcel._from}</td>
+                      <td>{parcel.destination}</td>
+                      <td>{parcel.weight}</td>
+                      <td>{parcel.price}</td>
+                      <td>{parcel.status}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
   );
 }
 
