61ff8374531756e81bd9fdf9779f9ab89dde245f
Ericawanja <wanjaerica@gmail.com>
2023-02-06 23:28:37 +0300

    sent and received endpoints integration

--- senditui/src/services/userParcels.service.js
+++ senditui/src/services/userParcels.service.js
@@ -2,29 +2,38 @@ import axios from "axios";
 
 class UserParcels {
   constructor() {
-    this.BASE_URL = `http://localhost:4000/profile/parcels`;
-    //${localStorage.getItem("token")}
+    this.BASE_URL = `http://localhost:4000/profile`;
+
     this.config = {
       headers: {
-        "mode":"cors",
-        "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        mode: "cors",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
         "Content-Type": "application/json",
       },
     };
   }
 
-  async GetAllUserParcels() {
-
+  async GetAllUserParcels(payload) {
+    let url;
+    if (payload === "received") {
+      url = `${this.BASE_URL}/parcels/received`;
+    } else if (payload === "sent") {
+      url = `${this.BASE_URL}/parcels/sent`;
+    } else {
+      url = `${this.BASE_URL}/parcels`;
+    }
 
     try {
-      let response = await axios.get(this.BASE_URL, this.config) ;
+      let response = await axios.get(url, this.config);
       return response;
     } catch (error) {
-    
+      console.log(error);
       let message = "An error occured";
-      return { data: null, error:message };
+      return { data: null, error: message };
     }
   }
+
+ 
 }
 
 export default new UserParcels();
