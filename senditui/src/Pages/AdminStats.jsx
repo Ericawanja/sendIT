520d7ed8e3e013ff318a8e4694d10d19877ca9ba
Ericawanja <wanjaerica@gmail.com>
2023-02-08 23:09:36 +0300

    stats container design

--- senditui/src/Pages/AdminStats.jsx
+++ senditui/src/Pages/AdminStats.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function AdminStats() {
   return (
-    <div>AdminStats</div>
-  )
+    <div className="statsContainer">
+      <div className="statsWrapper">
+        <div className="transit">
+          <span className="stat">1244</span>
+          <span className="smalltext">Parcels</span>
+          <span className="title">On Transit</span>
+        </div>
+        <div className="delivered">
+          <span className="stat">1244</span>
+          <span className="smalltext">Parcels</span>
+          <span className="title"> Delivered</span>
+        </div>
+        <div className="created">
+          <span className="stat">1244</span>
+          <span className="smalltext">Parcels</span>
+          <span className="title">created</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AdminStats
+export default AdminStats;
