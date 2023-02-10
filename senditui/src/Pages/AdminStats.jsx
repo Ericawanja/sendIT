8cbe4eedb42d373c457ae3f79ec6072b947dc98e
Ericawanja <wanjaerica@gmail.com>
2023-02-10 22:53:38 +0300

    admin

--- senditui/src/Pages/AdminStats.jsx
+++ senditui/src/Pages/AdminStats.jsx
@@ -1,21 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function AdminStats() {
+  const { parcels, loading } = useSelector((state) => state.adminData);
+  console.log(parcels);
+  // let onTransit = parcels?.map(p=>p.status === "on transit").length || 0
+  // let delivered = parcels?.map(p=>p.status === "delivered").length || 0
+  // let pending = parcels?.map(p=>p.status === "pending").length || 0
   return (
     <div className="statsContainer">
       <div className="statsWrapper">
         <div className="transit">
-          <span className="stat">1244</span>
+          <span className="stat">
+            {loading
+              ? loading
+              : parcels.map((p) => p.status === "on transit").length}
+          </span>
           <span className="smalltext">Parcels</span>
           <span className="title">On Transit</span>
         </div>
         <div className="delivered">
-          <span className="stat">1244</span>
+          <span className="stat">
+            {loading
+              ? loading
+              : parcels.map((p) => p.status === "delivered").length}
+          </span>
           <span className="smalltext">Parcels</span>
           <span className="title"> Delivered</span>
         </div>
         <div className="created">
-          <span className="stat">1244</span>
+          <span className="stat">
+          {loading
+              ? loading
+              : parcels.map((p) => p.status === "pending").length}
+          </span>
           <span className="smalltext">Parcels</span>
           <span className="title">created</span>
         </div>
